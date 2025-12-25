bdfb0a41499330096a986187b5ad06d8acb3dc75
Carrot <sannatanaka2000@gmail.com>
2025-12-24 21:30:03 -0600

    Refactor blog schema definition in config.ts test

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,7 @@
-import { z, defineCollection } from "astro:content";
-const blogSchema = z.object({
+import { defineCollection, z } from "astro:content";
+
+const blogSchema = defineCollection({
+  schema: z.object({
     title: z.string(),
     description: z.string(),
     pubDate: z.coerce.date(),
@@ -7,16 +9,11 @@ const blogSchema = z.object({
     heroImage: z.string().optional(),
     badge: z.string().optional(),
     tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
-        message: 'tags must be unique',
+      message: 'tags must be unique',
     }).optional(),
+  }),
 });
 
-export type BlogSchema = z.infer<typeof blogSchema>;
-
-
-const blogCollection = defineCollection({ schema: blogSchema });
-
-
 export const collections = {
-    'blog': blogCollection
-}
+    'blog': blogSchema,
+};
